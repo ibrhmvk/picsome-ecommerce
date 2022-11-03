aa93a119a08820edd1d2d0173d558282bb169a45
Ibrahim V K <89912630+ibrhmvk@users.noreply.github.com>
2022-11-03 19:16:45 +0530

    production

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import {BrowserRouter as Router} from "react-router-dom"
+import {ContextProvider} from "./Context"
+import App from "./App"
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ContextProvider>
+    <Router>
+      <App />
+    </Router>
+  </ContextProvider>
 )
